13380449dfb9a2e4517cd03731f0a1bfa661ad64
BAZ <118599888+BAZustc@users.noreply.github.com>
2023-10-23 02:32:52 +0800

    Update Panel.tsx

--- apps/enterprise/src/lib/ui/Panel/Panel.tsx
+++ apps/enterprise/src/lib/ui/Panel/Panel.tsx
@@ -30,55 +30,125 @@ export const Panel = styled.div<PanelProps>`
     const border = kind === 'secondary' ? '2px solid #e81cff' : '';
 
     return css`
-      &::before {
-        content: '';
-        position: absolute;
-        inset: 0;
-        left: -55px;
-        margin: auto;
-        width: 1px;
-        height: 1px;
-        border-radius: 1px;
-        background: ${contentBackground};
-        z-index: -10;
-        pointer-events: none;
-        transition: all 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-      }
+      .btn {
+  --border-color: linear-gradient(-45deg, #ffae00, #7e03aa, #00fffb);
+  --border-width: .125em;
+  --curve-size: .5em;
+  --blur: 30px;
+  --bg: #080312;
+  --color: #afffff;
+  color: var(--color);
+    /* use position: relative; so that BG is only for .btn */
+  position: relative;
+  isolation: isolate;
+  display: inline-grid;
+  place-content: center;
+  padding: .5em 1.5em;
+  font-size: 17px;
+  border: 0;
+  text-transform: uppercase;
+  box-shadow: 10px 10px 20px rgba(0, 0, 0, .6);
+  clip-path: polygon(
+            /* Top-left */
+            0% var(--curve-size),
 
-      &::after {
-        content: "";
-        z-index: -1;
-        position: absolute;
-        inset: 0;
-        background: ${contentBackground};
-        transform: translate3d(0, 0, 0) scale(0.95);
-        filter: blur(10px);
-      }
+            var(--curve-size) 0,
+            /* top-right */
+            100% 0,
+            100% calc(100% - var(--curve-size)),
 
-      ${border}
+            /* bottom-right 1 */
+            calc(100% - var(--curve-size)) 100%,
+            /* bottom-right 2 */
+            0 100%);
+  transition: color 250ms;
+}
 
-      .heading {
-        font-size: 20px;
-        text-transform: capitalize;
-        font-weight: 700;
-      }
+.btn::after,
+.btn::before {
+  content: '';
+  position: absolute;
+  inset: 0;
+}
 
-      p:not(.heading) {
-        font-size: 14px;
-      }
+.btn::before {
+  background: var(--border-color);
+  background-size: 300% 300%;
+  animation: move-bg7234 5s ease infinite;
+  z-index: -2;
+}
 
-      p:last-child {
-        color: #fff8fc;
-        font-weight: 600;
-      }
+@keyframes move-bg7234 {
+  0% {
+    background-position: 31% 0%
+  }
 
-      &:hover::after {
-        filter: blur(30px);
-      }
+  50% {
+    background-position: 70% 100%
+  }
 
-      &:hover::before {
-        transform: rotate(-90deg) scaleX(1.34) scaleY(0.77);
-      }
+  100% {
+    background-position: 31% 0%
+  }
+}
+
+.btn::after {
+  background: var(--bg);
+  z-index: -1;
+  clip-path: polygon(
+            /* Top-left */
+            var(--border-width) 
+            calc(var(--curve-size) + var(--border-width) * .5),
+
+            calc(var(--curve-size) + var(--border-width) * .5) var(--border-width),
+
+            /* top-right */
+            calc(100% - var(--border-width)) 
+            var(--border-width),
+
+            calc(100% - var(--border-width)) 
+            calc(100% - calc(var(--curve-size) + var(--border-width) * .5)),
+
+            /* bottom-right 1 */
+            calc(100% - calc(var(--curve-size) + var(--border-width) * .5)) calc(100% - var(--border-width)),
+            /* bottom-right 2 */
+            var(--border-width) calc(100% - var(--border-width)));
+  transition: clip-path 500ms;
+}
+
+.btn:where(:hover, :focus)::after {
+  clip-path: polygon(
+                /* Top-left */
+                calc(100% - var(--border-width)) 
+
+                calc(100% - calc(var(--curve-size) + var(--border-width) * 0.5)),
+    
+                calc(100% - var(--border-width))
+
+                var(--border-width),
+    
+                /* top-right */
+                calc(100% - var(--border-width))
+
+                 var(--border-width),
+    
+                calc(100% - var(--border-width)) 
+
+                calc(100% - calc(var(--curve-size) + var(--border-width) * .5)),
+    
+                /* bottom-right 1 */
+                calc(100% - calc(var(--curve-size) + var(--border-width) * .5)) 
+                calc(100% - var(--border-width)),
+
+                /* bottom-right 2 */
+                calc(100% - calc(var(--curve-size) + var(--border-width) * 0.5))
+                calc(100% - var(--border-width)));
+  transition: 200ms;
+}
+
+.btn:where(:hover, :focus) {
+  color: #fff;
+}
     `;
   }}
 `;
